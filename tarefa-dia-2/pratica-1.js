890a9d210c962ef034f013b730077cb1cd38c857
Evelyn Nunes <evelynrx_rj@hotmail.com>
2022-08-24 16:37:08 +0100

    atualizacao 3

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,5 +3,8 @@ let email=evelynrj_@hotmail.com
 let profissao=estudante
 let idade=37
 let sonho=maldivas
-
+console.log(evelyn)
+console.log(evelynrj_@hotmail.com)
+console.log(estudante)
+console.log(maldivas)
 
