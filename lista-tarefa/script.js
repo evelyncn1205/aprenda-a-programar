7aaedbb931010375a5215624363fdafb9e1c9136
Evelyn Nunes <evelynrx_rj@hotmail.com>
2022-08-26 01:05:30 +0100

    atualizandp aula 4

---
+++ lista-tarefa/script.js
@@ -0,0 +1,16 @@
+function adicionarTarefa(){
+    const item = document.createElement("li")
+
+item.innerHTML = `<li>
+<span>Tarefa 1</span>
+ <buttonclass="excluir2>x</button>
+ </li>`
+
+
+
+}
+
+adicionarTarefa()
+adicionarTarefa()
+adicionarTarefa()
+adicionarTarefa()
